86abfa8dc2738751a57fc5757dfd4d542bfc12da
Tobias <tobiasmaraci@gmx.de>
2023-10-04 18:16:14 +0200

    Added Unit Tests for the additional classes
    The Unit tests check if the file is read appropriately and additionally if countryAnalysis or weatherAnalysis do what they are supposed to

--- src/test/java/de/bcxp/challenge/AppTest.java
+++ src/test/java/de/bcxp/challenge/AppTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Example JUnit 5 test case.
@@ -20,6 +24,36 @@ class AppTest {
     @Test
     void aPointlessTest() {
         assertEquals("successful", successLabel, "My expectations were not met");
+    }
+
+    @Test
+    public void testWeather() throws IOException {
+        CsvReader reader = new CsvReader();
+        WeatherAnalysis lowestTemp = new WeatherAnalysis();
+
+        List<String[]> weatherData = reader.readFile("weather.csv");
+        assertNotNull(weatherData);
+
+        String dayWithSmallestSpread = lowestTemp.findSmallestTempSpread(weatherData);
+        assertNotNull(dayWithSmallestSpread);
+
+        int expectedDay = 14;
+        assertEquals(expectedDay, dayWithSmallestSpread);
+    }
+
+    @Test
+    public void testCountries() throws IOException {
+        CsvReader reader = new CsvReader();
+        CountryAnalysis highestDensity = new CountryAnalysis();
+
+        List<String[]> countryData = reader.readFile("countries.csv");
+        assertNotNull(countryData);
+
+        String countryWithHighestDensity = highestDensity.findCountryWithHighestDensity(countryData);
+        assertNotNull(countryWithHighestDensity);
+
+        String expectedCountryName = "CountryName";
+        assertEquals(expectedCountryName, countryWithHighestDensity);
     }
 
 }
